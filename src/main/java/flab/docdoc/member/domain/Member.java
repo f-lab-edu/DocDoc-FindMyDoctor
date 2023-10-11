043f15836591a62088b3fbe989734e214aa38c0c
HONGJIBEOM <corea1836@naver.com>
2023-10-12 06:05:42 +0900

    feat : Member 도메인 생성
    - Member Domain 생성
    - Member C/R/U API 생성
    - MySQL 설정

---
+++ src/main/java/flab/docdoc/member/domain/Member.java
@@ -0,0 +1,48 @@
+package flab.docdoc.member.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    public enum Role {
+        PUBLIC,
+        ADMIN
+    }
+
+    private Long uniqueId;
+
+    private Role role = Role.PUBLIC;
+
+    private String loginId;
+
+    private String password;
+
+    private String email;
+
+
+
+    @Builder(builderClassName = "AddMemberBuilder", builderMethodName = "AddMemberBuilder")
+    public Member(String loginId, String password, String email) {
+        Assert.notNull(loginId, "loginId must not be null");
+        Assert.notNull(password, "password must not be null");
+        this.loginId = loginId;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Builder(builderClassName = "UpdateMemberBuilder", builderMethodName = "UpdateMemberBuilder")
+    public Member(Long uniqueId,  String password, String email) {
+        Assert.notNull(uniqueId, "uniqueId must not be null");
+        Assert.notNull(password, "password must not be null");
+        this.uniqueId = uniqueId;
+        this.password = password;
+        this.email = email;
+    }
+
+}
