2f1ac8294fa3dee7c2c63a34997da7d21cd7ed2a
HONGJIBEOM <corea1836@naver.com>
2023-10-18 22:23:20 +0900

    feat : Hospital Admin API 개발
    - Hospital Admin save, delete API 개발
    - HospitalResponse(조회용 객체) 개발

---
+++ src/main/java/flab/docdoc/hospital/request/UpdateHospitalAdminRequest.java
@@ -0,0 +1,13 @@
+package flab.docdoc.hospital.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class UpdateHospitalAdminRequest {
+
+    @NotNull
+    private String hospitalUniqueId;
+    @NotNull
+    private Long memberUniqueId;
+}
