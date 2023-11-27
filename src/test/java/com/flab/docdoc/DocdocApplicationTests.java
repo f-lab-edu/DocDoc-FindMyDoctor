d1cf914292476b311a2785ec22b525882c896984
corea1836 <85447297+corea1836@users.noreply.github.com>
2023-11-27 19:27:08 +0900

    modify : test delete

--- src/test/java/com/flab/docdoc/DocdocApplicationTests.java
+++ src/test/java/com/flab/docdoc/DocdocApplicationTests.java
@@ -1,13 +1,13 @@
 package com.flab.docdoc;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DocdocApplicationTests {
+//@SpringBootTest
+//class DocdocApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
